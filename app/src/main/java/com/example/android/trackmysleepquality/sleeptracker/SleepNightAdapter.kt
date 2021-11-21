45ea1983713ff5b000cddb908d22b2a4961ad990
Kevin Beaton <kabton14@gmail.com>
2021-11-21 00:18:15 -0500

    Create the SleepNightAdapter class

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -15,3 +15,10 @@
  */
 
 package com.example.android.trackmysleepquality.sleeptracker
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.TextItemViewHolder
+
+class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>(){
+
+}
