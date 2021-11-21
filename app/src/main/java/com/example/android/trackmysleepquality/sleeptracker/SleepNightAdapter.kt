790ff0ea00e63ae6290cf3b9f32fe08c5282cb83
Kevin Beaton <kabton14@gmail.com>
2021-11-21 00:30:39 -0500

    Override onBindViewHolder()

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -24,4 +24,9 @@ class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>(){
     var data = listOf<SleepNight>()
 
     override fun getItemCount() = data.size
+
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+        val item = data(position)
+        holder.textView.text = item.sleepQuality.toString()
+    }
 }
