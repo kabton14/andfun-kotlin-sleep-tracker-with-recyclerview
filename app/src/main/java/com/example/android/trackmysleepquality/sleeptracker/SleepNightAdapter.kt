93711efb382b2314033d304e77b596880b4154bd
Kevin Beaton <kabton14@gmail.com>
2021-11-21 00:31:50 -0500

    Override onCreateViewHolder()

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.TextItemViewHolder
 import com.example.android.trackmysleepquality.database.SleepNight
@@ -28,5 +29,9 @@ class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>(){
     override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
         val item = data(position)
         holder.textView.text = item.sleepQuality.toString()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+        TODO("Not yet implemented")
     }
 }
