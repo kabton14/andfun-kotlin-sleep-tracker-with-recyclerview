c52461594a1494af1025c3b300a69eb2589c3def
Kevin Beaton <kabton14@gmail.com>
2021-11-22 17:28:33 -0500

    Update colors in onBindViewHolder() when view is recycled

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
@@ -35,6 +36,12 @@ class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>(){
 
     override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
         val item = data[position]
+        if (item.sleepQuality <= 1) {
+            holder.textView.setTextColor(Color.RED)
+        } else {
+            holder.textView.setTextColor(Color.BLACK)
+        }
+
         holder.textView.text = item.sleepQuality.toString()
     }
 
