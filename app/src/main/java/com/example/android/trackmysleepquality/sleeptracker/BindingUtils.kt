ed2d497306a019bfaff396269eb388e332bf208e
Kevin Beaton <kabton14@gmail.com>
2021-11-25 11:58:49 -0500

    Create binding adapters for views

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
@@ -15,3 +15,40 @@
  */
 
 package com.example.android.trackmysleepquality.sleeptracker
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.android.trackmysleepquality.convertDurationToFormatted
+import com.example.android.trackmysleepquality.convertNumericQualityToString
+import com.example.android.trackmysleepquality.database.SleepNight
+
+@BindingAdapter("sleepDurationFormatted")
+fun TextView.setSleepDuration(item: SleepNight?){
+    item?.let {
+        text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, context.resources)
+    }
+}
+
+@BindingAdapter("sleepQualityString")
+fun TextView.setSleepQualityString(item: SleepNight?){
+    item?.let {
+        text = convertNumericQualityToString(item.sleepQuality, context.resources)
+    }
+}
+
+@BindingAdapter("sleepImage")
+fun ImageView.setSleepImage(item: SleepNight?){
+    item?.let {
+        setImageResource(
+            when (item.sleepQuality) {
+                0 -> com.example.android.trackmysleepquality.R.drawable.ic_sleep_0
+                1 -> com.example.android.trackmysleepquality.R.drawable.ic_sleep_1
+                2 -> com.example.android.trackmysleepquality.R.drawable.ic_sleep_2
+                3 -> com.example.android.trackmysleepquality.R.drawable.ic_sleep_3
+                4 -> com.example.android.trackmysleepquality.R.drawable.ic_sleep_4
+                5 -> com.example.android.trackmysleepquality.R.drawable.ic_sleep_5
+                else -> com.example.android.trackmysleepquality.R.drawable.ic_sleep_active
+            })
+    }
+}
