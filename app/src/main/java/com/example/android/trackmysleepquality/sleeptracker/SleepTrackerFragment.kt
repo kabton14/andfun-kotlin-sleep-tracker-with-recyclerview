58bb54abfe8e46df5cb0c0da3b86b1ed1a700828
Kevin Beaton <kabton14@gmail.com>
2021-12-01 11:21:10 -0500

    Return the correct span size for each position

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -66,6 +66,10 @@ class SleepTrackerFragment : Fragment() {
 
         val manager = GridLayoutManager(activity, 3)
         manager.spanSizeLookup = object: GridLayoutManager.SpanSizeLookup(){
+            override fun getSpanSize(position: Int): Int = when (position) {
+                0 -> 3
+                else -> 1
+            }
 
         }
         binding.sleepList.layoutManager = manager
